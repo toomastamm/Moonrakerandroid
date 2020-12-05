d15c27a80a67aa9f394b50735d0ac1d3b91cc9dd
Toomas Tamm <toomastmm@gmail.com>
2020-12-05 18:16:51 +0200

    Add settings reloading and initial connection to moonraker

--- app/src/main/java/com/example/moonraker_android/MainActivity.kt
+++ app/src/main/java/com/example/moonraker_android/MainActivity.kt
@@ -1,22 +1,36 @@
 package com.example.moonraker_android
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.navigation.NavigationView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.preference.PreferenceManager
 import com.example.moonraker_android.ui.settings.SettingsActivity
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.result.Result
+import com.google.android.material.navigation.NavigationView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestSettings = 100
+    var connectionJob: Job? = null
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -40,9 +54,16 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        // Set default settings values
-        PreferenceManager.setDefaultValues(this,
-                R.xml.preferences, false);
+        // Set up preferences
+        var prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        PreferenceManager.setDefaultValues(
+            this,
+            R.xml.preferences, false
+        );
+
+        // Connect to moonraker
+        connectToMoonraker(prefs)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -59,9 +80,76 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.action_settings) {
             val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
-            return true
+            startActivityForResult(intent, requestSettings);
         }
-        return false
+        return super.onOptionsItemSelected(item);
     }
+
+    private fun connectToMoonraker(prefs: SharedPreferences?): Job? {
+        if (prefs == null) {
+            return null
+        }
+
+        Log.d(
+            "PrinterConnect", "Connecting to http://${
+                prefs.getString(
+                    "moonraker_ip",
+                    "undefined"
+                )
+            }:${prefs.getString("moonraker_port", "undefined")}"
+        )
+
+        val httpAsync = (
+                "http://" +
+                        prefs.getString("moonraker_ip", "undefined") + ":" +
+                        prefs.getString("moonraker_port", "undefined") +
+                        "/printer/info"
+                ).httpGet()
+            .responseJson() { _, _, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        val ex = result.getException()
+                        Log.e("firstGet", ex.toString())
+
+                        val toast = Toast.makeText(
+                            applicationContext,
+                            "Could not connect to http://${
+                                prefs.getString(
+                                    "moonraker_ip",
+                                    "undefined"
+                                )
+                            }:${prefs.getString("moonraker_port", "undefined")}",
+                            Toast.LENGTH_LONG
+                        )
+                        toast.show()
+                    }
+                    is Result.Success -> {
+                        val data = result.get().obj()
+                        val toast = Toast.makeText(
+                            applicationContext,
+                            data.getJSONObject("result").getString("state_message"),
+                            Toast.LENGTH_SHORT
+                        )
+                        toast.show()
+                    }
+                }
+            }
+
+        return MainScope().launch(Dispatchers.IO) {
+            httpAsync.join()
+        }
+    }
+
+    // Recreates activity on settings change
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            requestSettings -> {
+                Log.d("activityResult", "recreating")
+                // Recreate activity
+                recreate()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/moonraker_android/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/moonraker_android/ui/home/HomeFragment.kt
@@ -1,15 +1,17 @@
 package com.example.moonraker_android.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.preference.PreferenceManager
 import com.example.moonraker_android.R
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result;
 
 class HomeFragment : Fragment() {
 
@@ -26,9 +28,6 @@ class HomeFragment : Fragment() {
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        //homeViewModel.text.observe(viewLifecycleOwner, Observer {
-        //    textView.text = it
-        //})
 
         textView.text = getString(R.string.prefs_test_string, prefs.getString("moonraker_ip", "undefined"), prefs.getString("moonraker_port", "undefined"))
 
