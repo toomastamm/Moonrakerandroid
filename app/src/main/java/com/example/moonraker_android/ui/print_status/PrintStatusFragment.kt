9ce09b4d31cd8f9d8f0c85673f41222ae5d9a499
mandzeete <aivar.loopalu@gmail.com>
2020-12-05 18:57:47 +0200

    Removed Home and made Print Status as default.

--- app/src/main/java/com/example/moonraker_android/ui/print_status/PrintStatusFragment.kt
+++ app/src/main/java/com/example/moonraker_android/ui/print_status/PrintStatusFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.preference.PreferenceManager
 import com.example.moonraker_android.R
 
 class PrintStatusFragment : Fragment() {
@@ -19,12 +20,14 @@ class PrintStatusFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+
         printStatusViewModel =
             ViewModelProviders.of(this).get(PrintStatusViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_print_status, container, false)
         val textView: TextView = root.findViewById(R.id.text_print_status)
         printStatusViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            textView.text = getString(R.string.prefs_test_string, prefs.getString("moonraker_ip", "undefined"), prefs.getString("moonraker_port", "undefined"))
         })
         return root
     }
