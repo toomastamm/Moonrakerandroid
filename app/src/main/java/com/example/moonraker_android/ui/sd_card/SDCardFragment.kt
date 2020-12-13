7d3cadb97b637f5fe6ccc2771bd037ff22c44e59
mandzeete <aivar.loopalu@gmail.com>
2020-12-14 00:45:52 +0200

    Replaced androidx.lifecycle with ViewModeProvider way in SDCardFragment. Added Room DB. After merge.

--- app/src/main/java/com/example/moonraker_android/ui/sd_card/SDCardFragment.kt
+++ app/src/main/java/com/example/moonraker_android/ui/sd_card/SDCardFragment.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.work.*
 import com.example.moonraker_android.R
 import com.example.moonraker_android.worker.PrintWorker
@@ -16,9 +16,8 @@ import kotlinx.android.synthetic.main.fragment_sd_card.*
 
 class SDCardFragment : Fragment() {
 
-    private val viewModel: SDCardViewModel by activityViewModels()
-    private val fileMap = hashMapOf<String, SDCardFileResponse>()
     private var fileList = arrayListOf<String>()
+    private lateinit var viewModel: SDCardViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,12 +25,21 @@ class SDCardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
+
+        return inflater.inflate(R.layout.fragment_sd_card, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(SDCardViewModel::class.java)
+
         viewModel.state.observe(viewLifecycleOwner, { item ->
             fileList = arrayListOf() // reset the list when new files are added to the SD card
 
             for (i in 0 until item.size) {
                 val file = item[i]
-                fileMap[file.filename] = file
+                viewModel.addFile(file)
                 fileList.add(file.filename)
             }
             setFileInfo(fileList[0])
@@ -67,11 +75,6 @@ class SDCardFragment : Fragment() {
             text_slicer_version.text = item.slicerVersion
             text_estimated_time.text = item.estimatedTime
         })
-        return inflater.inflate(R.layout.fragment_sd_card, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
         printButton.setOnClickListener {
             startPrint(fileList[spinner_files.selectedItemPosition])
@@ -94,8 +97,9 @@ class SDCardFragment : Fragment() {
     }
 
     private fun setFileInfo(fileName: String) {
-        text_modified.text = fileMap[fileName]?.modified.toString()
-        text_size.text = fileMap[fileName]?.size.toString()
+        val file = viewModel.getFileByName(fileName)
+        text_modified.text = file.modified
+        text_size.text = file.size
     }
 
     private fun updateFilesState() {
