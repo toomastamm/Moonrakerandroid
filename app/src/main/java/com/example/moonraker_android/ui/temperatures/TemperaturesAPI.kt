bb05c4919c3c3e494223819119d7d4c3daca3f12
Toomas Tamm <toomastmm@gmail.com>
2020-12-13 19:03:05 +0200

    Add live temperature viewing and setting support

--- app/src/main/java/com/example/moonraker_android/ui/temperatures/TemperaturesAPI.kt
+++ app/src/main/java/com/example/moonraker_android/ui/temperatures/TemperaturesAPI.kt
@@ -2,7 +2,9 @@ package com.example.moonraker_android.ui.temperatures
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.example.moonraker_android.ui.motors.MotorsAPI
 import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.fuel.json.responseJson
 import com.github.kittinunf.result.Result
 
@@ -58,7 +60,7 @@ object TemperaturesAPI {
 
                         for (extruder in extruders) {
                             val currentExtruder = status.getJSONObject(extruder)
-                            heaterBedsTemperatures[extruder] = ObjectTemperatures(
+                            extrudersTemperatures[extruder] = ObjectTemperatures(
                                 current = currentExtruder.getDouble("temperature"),
                                 target = currentExtruder.getDouble("target")
                             )
@@ -73,6 +75,24 @@ object TemperaturesAPI {
 
                         Log.d(TAG, resp.toString())
                         _state.postValue(resp)
+                    }
+                }
+            }
+    }
+
+    fun setTemperatureTarget(name: String, target: String) {
+        var path = "/printer/gcode/script?script=SET_HEATER_TEMPERATURE HEATER=$name TARGET=$target"
+
+        path.httpPost().also { Log.d(TAG, "Loading state from ${it.url}") }
+            .responseJson { _, _, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        val ex = result.getException()
+                        Log.e(TAG, "Error response: $ex")
+                    }
+                    is Result.Success -> {
+                        val data = result.get().obj()
+                        Log.d(TAG, "Response to the POST request: $data")
                     }
                 }
             }
--- app/src/main/java/com/example/moonraker_android/ui/temperatures/TemperaturesFragment.kt
+++ app/src/main/java/com/example/moonraker_android/ui/temperatures/TemperaturesFragment.kt
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.moonraker_android.MainActivity
 import com.example.moonraker_android.R
+import com.example.moonraker_android.api.MoonrakerService
+import kotlinx.android.synthetic.main.fragment_print_status.*
+import kotlinx.android.synthetic.main.fragment_temperatures.*
 
 class TemperaturesFragment : Fragment() {
 
@@ -23,11 +26,37 @@ class TemperaturesFragment : Fragment() {
     ): View? {
         mainActivity = activity as MainActivity
 
+        viewModel.state.observe(viewLifecycleOwner, { item ->
+            item.heater_beds["heater_bed"]?.current.let {
+                plate_temp.text = String.format("%.2f C", it)
+            }
+            item.heater_beds["heater_bed"]?.target.let {
+                plate_target.text = String.format("%.2f C", it)
+            }
+            item.extruders["extruder"]?.current.let {
+                hotend_temp.text = String.format("%.2f C", it)
+            }
+            item.extruders["extruder"]?.target.let {
+                hotend_target.text = String.format("%.2f C", it)
+            }
+        })
+
+
+
         return inflater.inflate(R.layout.fragment_temperatures, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        button_set_hotend.setOnClickListener {
+            TemperaturesAPI.setTemperatureTarget("extruder", editText_hotend.text.toString())
+        }
+
+        button_set_plate.setOnClickListener {
+            TemperaturesAPI.setTemperatureTarget("heater_bed", editText_plate.text.toString())
+        }
+
 
         updateState()
     }
